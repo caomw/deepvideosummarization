4ae950231d27eb54cd89e2a52237aed072158e3a
ericlin79119 <ericlin79119@gmail.com>
2015-04-07 19:19:53 -0400

    GT labels and code to generate

---
+++ genGTframeNum.m
@@ -0,0 +1,21 @@
+userSummDirName='newUserSummary';
+userSummDir=dir(userSummDirName);
+gtLabels=cell(length(userSummDir)-2,2);
+gtLabels(:,1)=arrayfun(@(X) X.name,userSummDir(3:end,1),'uniformoutput',false);
+for v=3:length(userSummDir)
+    userDirName=[userSummDirName,'/',userSummDir(v).name];
+    userDir=dir(userDirName);
+    if(strcmp(userDir(3).name,'Thumbs.db'))
+        userDir(3)=[];
+    end
+    gtLabels{v-2,2}=cell(length(userDir)-2,1);
+    for u=3:length(userDir)
+        gtDirName=[userDirName,'/',userDir(u).name];
+        gtDir=dir(gtDirName);
+        if(strcmp(gtDir(3).name,'Thumbs.db'))
+            gtDir(3)=[];
+        end
+        gtLabels{v-2,2}{u-2}=sort(cell2mat(arrayfun(@(X) str2double(X.name(6:end-4)),gtDir(3:end),'uniformoutput',false)),'ascend');
+    end
+end
+save('gtLabels.mat','gtLabels');
